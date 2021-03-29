503fe21d715ebf5389271a159ae027664a46c7a2
Timo Nicolai <timonicolai@arcor.de>
2021-03-29 16:05:12 +0200

    C: is_record_ref -> is_record_indirection

--- wrap/c_type_translation_rules.py
+++ wrap/c_type_translation_rules.py
@@ -6,14 +6,14 @@ def is_cap(t):
     return t.is_record() and t.is_template_instantiation('L4::Cap')
 
 
-def is_cap_ref(t):
-    return t.is_record_ref() and t.pointee().is_template_instantiation('L4::Cap')
+def is_cap_indirection(t):
+    return t.is_record_indirection() and t.pointee().is_template_instantiation('L4::Cap')
 
 
 class CTypeTranslator(TypeTranslator):
     rule = TypeTranslator.rule
 
-    @rule(lambda t: is_cap(t) or is_cap_ref(t))
+    @rule(lambda t: is_cap(t) or is_cap_indirection(t))
     def target(cls, t, args):
         return 'l4_cap_idx_t'
 
@@ -21,7 +21,7 @@ class CTypeTranslator(TypeTranslator):
     def input(cls, t, args):
         return f"{{interm}} = {t}({{inp}});"
 
-    @rule(lambda t: is_cap_ref(t))
+    @rule(lambda t: is_cap_indirection(t))
     def input(cls, t, args):
         return code(
             f"""
@@ -33,6 +33,6 @@ class CTypeTranslator(TypeTranslator):
     def output(cls, t, args):
         return "return {outp}.cap();"
 
-    @rule(lambda t: is_cap_ref(t))
+    @rule(lambda t: is_cap_indirection(t))
     def output(cls, t, args):
         return "return {outp}->cap();"
